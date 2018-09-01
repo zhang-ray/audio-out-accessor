6124951e1ba3cedd99982a255ac092e859836673
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-09-02 01:17:42 +0800

    resolve #1: make AppImage for oscilloscope

--- scripts/ubuntu.make.appimage.sh
+++ scripts/ubuntu.make.appimage.sh
@@ -2,10 +2,14 @@ pwd
 mkdir AppDir
 mkdir AppDir/usr/
 mkdir AppDir/usr/bin/
-cp ../build/examples/01.oscilloscope AppDir/AppRun
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64" -O AppDir/AppRun 
+chmod a+x AppDir/AppRun
 cp ../build/examples/01.oscilloscope AppDir/usr/bin/oscilloscope
-echo "[Desktop Entry]\nName=oscilloscope\nExec=oscilloscope\nIcon=oscilloscope\nType=Application" > AppDir/oscilloscope.desktop
+echo -e "[Desktop Entry]\nName=oscilloscope\nExec=oscilloscope\nIcon=oscilloscope\nType=Application" > AppDir/oscilloscope.desktop
 convert -size 256x256 xc:transparent AppDir/oscilloscope.png
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+chmod a+x linuxdeploy-x86_64.AppImage
+./linuxdeploy-x86_64.AppImage --appdir=AppDir
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage AppDir
