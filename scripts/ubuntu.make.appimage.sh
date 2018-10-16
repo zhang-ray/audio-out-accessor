645020e6153898debd344ffefef3775d5d7b456a
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-16 14:13:32 +0800

    fix #2: desktop file lacks Categories=

--- scripts/ubuntu.make.appimage.sh
+++ scripts/ubuntu.make.appimage.sh
@@ -5,7 +5,7 @@ mkdir AppDir/usr/bin/
 wget "https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64" -O AppDir/AppRun 
 chmod a+x AppDir/AppRun
 cp ../build/examples/01.oscilloscope AppDir/usr/bin/oscilloscope
-echo -e "[Desktop Entry]\nName=oscilloscope\nExec=oscilloscope\nIcon=oscilloscope\nType=Application" > AppDir/oscilloscope.desktop
+echo -e "[Desktop Entry]\nName=oscilloscope\nExec=oscilloscope\nIcon=oscilloscope\nType=Application\nCategories=AudioVideo;Audio;" > AppDir/oscilloscope.desktop
 convert -size 256x256 xc:transparent AppDir/oscilloscope.png
 wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
 chmod a+x linuxdeploy-x86_64.AppImage
